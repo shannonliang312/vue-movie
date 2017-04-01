97bac764c21ab13f765a623e8487656d2c16b94d
shannonliang <shannonliang312@gmail.com>
2017-04-01 15:16:42 +0800

    去掉导演样式

--- src/components/movie-card.vue
+++ src/components/movie-card.vue
@@ -9,7 +9,7 @@
         <div>原名： {{movieInfo.original_title}}</div>
         <div>类型： {{movieInfo.subtype}}</div>
         <div>上映时间： {{movieInfo.year}}</div>
-        <div>导演：<span class="human" v-for="director in movieInfo.directors">{{director.name}}&nbsp;&nbsp;</span></div>
+        <div>导演：<span v-for="director in movieInfo.directors">{{director.name}}&nbsp;&nbsp;</span></div>
         <div>主演：<span class="human" v-for="cast in movieInfo.casts" @click="showCastInfo(cast)">{{cast.name}}&nbsp;&nbsp;</span></div>
         <div>标签：<span v-for="genre in movieInfo.genres">{{genre}}&nbsp;&nbsp;</span></div>
         <div>收藏数量： {{movieInfo.collect_count}}</div>
